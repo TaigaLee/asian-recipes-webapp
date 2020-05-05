128c781d4e4639a221f2b28c5ff1dc56985909e6
taigalee <taiga@taigalee.com>
2020-05-05 13:17:54 -0500

    deployment

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def after_request(response):
   return response
 
 if 'ON_HEROKU' in os.environ:
-  print('\non heroku!')
+  print('non heroku!')
   models.initialize()
 
 if __name__ == '__main__':
